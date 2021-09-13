176148abf6b9f278f62d4283e205bc885621f9fd
Cristian Moreno <cristian.moreno@terpel.com>
2021-09-13 07:49:12 -0500

    Add github pages

---
+++ Proyectos/04-gif-expert-app/docs/precache-manifest.e7df4782dee43d67842fae7ee68eb18f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a61305c03938772090d107d97f2bb33b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e063c02690cb8d800bb",
+    "url": "/static/css/main.7ab33465.chunk.css"
+  },
+  {
+    "revision": "e107496e2e01402b74e7",
+    "url": "/static/js/2.b11609f6.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.b11609f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e063c02690cb8d800bb",
+    "url": "/static/js/main.3d122ee4.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
